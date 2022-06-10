f8987f689d69989282ced5eab36efbfb48c71956
OlyaPash <olya.pash.rb@gmail.com>
2022-06-10 21:55:30 +0500

    поправила отступы

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   def tests_level(level)
     Test.joins('JOIN user_passed_tests ON user_passed_tests.test_id = tests.id')
-    .where(tests: { level: level} )
-    .where(user_passed_tests: { user_id: id })
+      .where(tests: { level: level} )
+      .where(user_passed_tests: { user_id: id })
   end
 end
