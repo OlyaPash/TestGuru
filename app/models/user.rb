ae02a0205b25acbe79584191c7a8eeeca3b97195
OlyaPash <olya.pash.rb@gmail.com>
2022-06-14 22:54:43 +0500

    поправила модели, seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  has_many :tests, class_name: 'Test', foreign_key: 'author_id'
-  has_many :user_passed_tests
+  has_many :author_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :user_passed_tests, dependent: :destroy
   has_many :tests, through: :user_passed_tests
 
   def tests_level(level)
-    Test.joins('JOIN user_passed_tests ON user_passed_tests.test_id = tests.id')
-      .where(tests: { level: level} )
-      .where(user_passed_tests: { user_id: id })
+    tests.where(level: level)
   end
 end
