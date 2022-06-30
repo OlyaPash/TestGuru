50f1d6c80f00626965dfe46a4dea9ad1996a9f4d
OlyaPash <olya.pash.rb@gmail.com>
2022-06-30 14:41:05 +0500

    сгенерены файлы для ответов + fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   resources :tests do
-    resources :questions, except: [:index], shallow: true
+    resources :questions, shallow: true, except: [:index] do
+      resources :answers, shallow: true
+    end  
   end
 end
