5bad00ad2f4f280e125923ae3c260ada5a99826b
OlyaPash <olya.pash.rb@gmail.com>
2022-07-01 18:06:46 +0500

    работа с test_passage & views

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,11 +3,58 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  # before_validation :before_validation_set_first_question, on: :create
+  before_validation :next_question
+
+  SUCCESS_PERCENT = 85
+
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    save!
+  end
+
+  def correct_percent
+    100 / test.questions.count * correct_questions
+  end
+
+  def number_current_questions
+    test.questions.where('id <= ?', current_question.id).count
+  end
+
+  def done?
+    correct_percent >= SUCCESS_PERCENT
+  end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  # def before_validation_set_first_question
+  #   self.current_question = test.questions.first if test.present?
+  # end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    self.current_question = if new_record?
+      test.questions.order(:id).first if test.present?
+    else
+      test.questions.order(:id).where('id > ?', current_question.id).first
+    end
+  end
+
+  # def before_validation_set_next_question
+  #   self.current_question = next_question
+  # end
 end
