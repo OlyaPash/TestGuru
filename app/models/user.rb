7a4a68a13807bba01823a7bb91d420bd5ccdb42e
OlyaPash <olya.pash.rb@gmail.com>
2022-07-12 23:11:42 +0500

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :author_tests, class_name: 'Test', foreign_key: 'author_id'
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  has_secure_password
-
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :email, presence: true,
                     uniqueness: { scope: :email, message: "This email is already registered" },
                     format: {with: URI::MailTo::EMAIL_REGEXP, message: "Wrong format"}
