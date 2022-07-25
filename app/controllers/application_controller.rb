83fef55f74c1d7c9c36e0c2324e95f6e76c64ab0
OlyaPash <olya.pash.rb@gmail.com>
2022-07-25 14:12:10 +0500

    less1-localiz_in_rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
@@ -8,6 +9,16 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(_resource)
     current_user.is_a?(Admin) ? admin_tests_path : tests_path
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
