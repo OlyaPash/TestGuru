1997ea60f62a156c9dc30e35e8cd953750f22856
OlyaPash <olya.pash.rb@gmail.com>
2022-10-26 13:12:50 +0500

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_production"
 
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'evening-shore-22210.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
