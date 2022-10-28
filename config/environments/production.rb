abc002db0de12d57b3566fbfaa88cd42f522173c
OlyaPash <olya.pash.rb@gmail.com>
2022-10-28 15:24:13 +0500

    yandex fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,6 @@ Rails.application.configure do
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
-    tls:                  true,
     ssl:                  true,
     enable_starttls_auto: true }
 
