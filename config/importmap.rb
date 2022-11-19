f9d2c157763f8df899c4226991f35a7c89a214cf
OlyaPash <olya.pash.rb@gmail.com>
2022-11-19 14:32:02 +0500

    fix importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/utilities", under: "utilities"
+pin "check_password"
+pin "form_inline"
+pin "progress_bar"
+pin "sorting"
