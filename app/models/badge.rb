ec99ec4c51c1fe9838e5e168312cd78180b221cf
OlyaPash <olya.pash.rb@gmail.com>
2022-12-08 12:19:58 +0500

    add dependent

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,5 +1,5 @@
 class Badge < ApplicationRecord
-  has_many :user_badges
+  has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
   TYPE_BADGES = %w[category level attempts].freeze
